de10464f5f274dc4f1f83475cac68473904274e1
Jue Shi <jueshi@gmail.com>
2025-08-18 08:12:19 -0700

    Apply .gitignore and remove large files

--- stock_charts_10k10q/path_fix.py
+++
@@ -1,7 +0,0 @@
-import sys
-import site
-
-# Add user site-packages to path
-user_site = site.getusersitepackages()
-if user_site not in sys.path:
-    sys.path.append(user_site)
--- stock_charts_10k10q/test_print.py
+++
@@ -1,7 +0,0 @@
-"""
-Simple test script to verify Python execution and output
-"""
-print("Starting test script...")
-print("This is a test to verify Python script execution")
-print("If you can see this output, the script is executing correctly")
-print("Test complete")
